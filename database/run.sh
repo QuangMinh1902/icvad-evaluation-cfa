1cc3fb745af4b87bc62182571cae02d8e71e3c62
Quang Minh PHAM <quangminh1575@gmail.com>
2024-05-24 11:42:59 +0200

    update

--- database/run.sh
+++ database/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker start counter-database 1> /dev/null 2> /dev/null
 if [ $? -ne 0 ]; then
-  docker run -p 5432:5432 --name counter-database -d -t --restart unless-stopped counter-database
+  docker run -p 5433:5432 --name counter-database -d -t --restart unless-stopped counter-database
 echo "Start successful"
+fi
